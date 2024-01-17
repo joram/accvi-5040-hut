78c0f7e40a88d41d421d1fe6dfc36da83d3a5608
John Oram <john@oram.ca>
2024-01-16 20:30:56 -0800

    tweak

--- webcam/push_to_accvi.sh
+++ webcam/push_to_accvi.sh
@@ -3,4 +3,4 @@
 cd /home/pi/accvi-5040-hut/data/webcam
 lastimage=`ls -tr . | tail -1`
 echo $lastimage
-scp  -i ${ID_RSA_FILEPATH} $lastimage accvica@accvi.ca:www/wp-content/hutimages/newest.jpeg
+scp -i ${ID_RSA_FILEPATH} $lastimage accvica@accvi.ca:www/wp-content/hutimages/newest.jpeg
