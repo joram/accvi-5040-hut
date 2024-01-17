ff15c00a259dc7f992ef09a3c096265dc43e2971
John Oram <john@oram.ca>
2024-01-16 20:32:31 -0800

    tweak

--- webcam/push_to_accvi.sh
+++ webcam/push_to_accvi.sh
@@ -4,4 +4,4 @@ export $(grep -v '^#' .env | xargs -0)  # Load environment variables
 cd /home/pi/accvi-5040-hut/data/webcam
 lastimage=`ls -tr . | tail -1`
 echo $lastimage
-scp -i ${ID_RSA_FILEPATH} $lastimage accvica@accvi.ca:www/wp-content/hutimages/newest.jpeg
+scp -i ${ID_RSA_FILEPATH} "$lastimage" accvica@accvi.ca:www/wp-content/hutimages/newest.jpeg
