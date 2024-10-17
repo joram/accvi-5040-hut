629fd57eb0fd299cc91ea101960c1feec66058a2
John Oram <john@oram.ca>
2024-10-17 09:56:36 -0700

    organize the S3 files

--- webcam/push_to_s3.sh
+++ webcam/push_to_s3.sh
@@ -2,6 +2,7 @@
 # This is meant to be run from the root of the project
 export $(grep -v '^#' .env | xargs -0)  # Load environment variables
 
-cd ./data/webcam/
-# if there is any tar.gz files, send them to s3
-aws s3 sync --follow-symlinks --include *.jpg . s3://5040-hut-data.oram.ca/webcam/ # Sync data to S3
+# push jpg files to S3 (resume failed previous pushes)
+TODAYS_DATE=$(date -d "today 13:00" '+%Y-%m-%d')
+cd /home/pi/accvi-5040-hut/data/webcam/$TODAYS_DATE
+aws s3 sync --follow-symlinks . s3://5040-hut-data.oram.ca/webcam/$TODAYS_DATE/
