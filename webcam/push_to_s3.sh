1f357b89550a8f893d1be3d592a4ad01f57effdf
John Oram <john@oram.ca>
2024-10-17 09:52:01 -0700

    update cron

--- webcam/push_to_s3.sh
+++ webcam/push_to_s3.sh
@@ -2,13 +2,6 @@
 # This is meant to be run from the root of the project
 export $(grep -v '^#' .env | xargs -0)  # Load environment variables
 
-# if there is no tar.gz files, exit
-if [ ! "$(ls -A ./data/webcam/*.tar.gz)" ]; then
-    echo "No files to send to S3"
-    exit 0
-fi
-export $(grep -v '^#' .env | xargs -0)  # Load environment variables
-
 cd ./data/webcam/
 # if there is any tar.gz files, send them to s3
 aws s3 sync --follow-symlinks --include *.jpg . s3://5040-hut-data.oram.ca/webcam/ # Sync data to S3
