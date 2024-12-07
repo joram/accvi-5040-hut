c383b9da9e223dc8a09d019a076251509ec0c1a0
John Oram <john@oram.ca>
2024-12-06 16:23:39 -0800

    ignore snowdepth when pushing to accvi

--- webcam/push_to_accvi.sh
+++ webcam/push_to_accvi.sh
@@ -3,6 +3,7 @@
 export $(grep -v '^#' .env | xargs -0)  # Load environment variables
 TODAYS_DATE=$(date -d "today 13:00" '+%Y-%m-%d')
 cd /home/pi/accvi-5040-hut/data/webcam/$TODAYS_DATE
-lastimage=`ls -tr . | tail -1`
+lastimage=$(find . -type f ! -name '*_snow_depth.jpg' ! -name '*.txt' -exec stat -c "%Y %n" {} + 2>/dev/null | sort -n | tail -n 1 | cut -d' ' -f2-)
+
 echo $lastimage
 scp -i ${ID_RSA_FILEPATH} "./$lastimage" accvica@accvi.ca:www/wp-content/hutimages/newest.jpeg
